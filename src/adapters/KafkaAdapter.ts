cec3b245aad1747b698f5f7162510f9ac58d72a8
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-11-15 11:13:49 +0000

    feat: set up kafka

---
+++ src/adapters/KafkaAdapter.ts
@@ -0,0 +1,53 @@
+import { IMessageConsumer, IMessageProducer } from "@/types";
+import Kafka from "kafkajs";
+
+export class KafkaProducer implements IMessageProducer {
+    private producer: Kafka.Producer;
+
+    constructor(kafka: Kafka.Kafka) {
+        this.producer = kafka.producer();
+    }
+
+    async connect(): Promise<void> {
+        await this.producer.connect();
+    }
+
+    async disconnect(): Promise<void> {
+        await this.producer.disconnect();
+    }
+
+    async send(topic: string, key: string, value: string): Promise<void> { 
+        await this.producer.send({
+            topic,
+            messages: [{ key, value }],
+        });
+    }
+}
+
+export class KafkaConsumer implements IMessageConsumer {
+    private consumer: Kafka.Consumer;
+
+    constructor(kafka: Kafka.Kafka, groupId: string) {
+        this.consumer = kafka.consumer({ groupId });
+    }
+
+    async connect(): Promise<void> {
+        await this.consumer.connect();
+    }
+
+    async disconnect(): Promise<void> {
+        await this.consumer.disconnect();
+    }
+
+    async subscribe(topic: string, fromBeginning?: boolean): Promise<void> {
+        await this.consumer.subscribe({ topic, fromBeginning });
+    }
+
+    async onMessage(handler: (message: any) => Promise<void>): Promise<void> {
+        await this.consumer.run({
+            eachMessage: async ({ message }) => {
+                await handler(message);
+            },
+        });
+    }
+}
--- src/types.ts
+++ src/types.ts
@@ -19,3 +19,20 @@ export type Trade = {
     buyCurrency: string;
 	timestamp: number;
 };
+
+export interface IMessageProducer {
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    send(topic: string, key:string, value:string): Promise<void>;
+}
+
+export interface IMessageConsumer {
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    subscribe(topic: string, fromBeginning?: boolean): Promise<void>;
+    onMessage(handler: (message: any) => Promise<void>): Promise<void>;
+}
+
+export interface ITradeIdGenerator {
+    generate(): Promise<string>;
+}
