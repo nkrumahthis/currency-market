f937b445dff947a6e2cf769f0684549ae164f23d
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-12-19 11:19:11 +0000

    update cta link

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -88,7 +88,7 @@ const Home = () => {
                   <div className="flex flex-col mt-5 sm:mt-8 gap-4">
                     <div className="sm:flex sm:justify-center lg:justify-start">
                       <div className="rounded-md shadow flex space-x-8">
-                        <Link href={"/customers"} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
+                        <Link href={"/invoice"} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
                           Pay Your Invoice
                         </Link>
                       </div>
